91a0ded6d24b9eb48aa948e395e8dcbf4d5df763
Rivo Raoel (rivony) <rivony@gmail.com>
2018-11-23 18:28:22 +0400

    feat: bcde ko

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,12 +1,25 @@
 ﻿function sendRequest() {
-  var url = document.getElementById("url").value;
-  document.write("Redirecting to the url in 3 seconds...");
+  var url = document.getElementById("url").value
+  document.write("Redirecting to the url in 2 seconds...")
   setTimeout(function() {
-    window.location = url;
-  }, 3000);
+    window.location = url
+  }, 2000)
 }
 
 function setUrl(value) {
   document.getElementById("url").value =
-    document.getElementById("baseUrl").value + "?simulate=" + value;
+    document.getElementById("baseUrl").value + "?simulate=" + value
+}
+
+function toggleSections(value) {
+	var koSections = document.querySelectorAll('[id$="-ko-section"]')
+	koSections.forEach(function(koSection) {
+		koSection.hidden = true
+	})
+	var section = document.getElementById(value + "-section")
+	section.hidden = false
+}
+
+function replicateUrl(baseUrl){
+	document.getElementById("url").value = baseUrl + "?" + document.getElementById("url").value.split('?')[1]
 }
