02eccc01d87945880a09b76d907747b18762d882
JL52059332 <Jeremie.litzler@conduent.com>
2018-11-23 08:05:34 +0100

    feat: commit initial code

---
+++ js/JavaScript.js
@@ -0,0 +1,12 @@
+﻿function sendRequest() {
+  var url = document.getElementById("url").value;
+  document.write("Redirecting to the url in 3 seconds...");
+  setTimeout(function() {
+    window.location = url;
+  }, 3000);
+}
+
+function setUrl(value) {
+  document.getElementById("url").value =
+    document.getElementById("baseUrl").value + "?simulate=" + value;
+}
