2f47e2d6d2a90a24cbfe5abb1bba1cc2699453c0
Rivo Raoel (rivony) <rivony@gmail.com>
2018-12-11 08:01:27 +0400

    chore: suffix

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -11,12 +11,14 @@ function setSsoCookie(){
   var userToken = document.getElementById("userToken").value
   var cookieKey = encodeURIComponent("ssotoken") + "="
   var expiredOn = "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
+  var cookieSuffix = expiredOn
   var domain = ";domain=etude.tr"
+  cookieSuffix += domain
   if(userToken){
-	document.cookie = cookieKey + userToken + expiredOn
+	document.cookie = cookieKey + userToken + cookieSuffix
   }
   else{
-	  document.cookie = cookieKey + "b45a2b0774394b48922994f6f8e6180e" + expiredOn
+	  document.cookie = cookieKey + "b45a2b0774394b48922994f6f8e6180e" + cookieSuffix
   }
   console.log(document.cookie)	
 }
